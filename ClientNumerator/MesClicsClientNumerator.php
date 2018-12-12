945095c46473cbc7e80235fe55f913324e5f2689
MesClics <mesclics@hotmail.com>
2018-12-12 18:08:43 +0100

    debug: client dont initiales n'ont aps encore été attribuées pour numéro auto

--- ClientNumerator/MesClicsClientNumerator.php
+++ ClientNumerator/MesClicsClientNumerator.php
@@ -65,10 +65,13 @@ class MesClicsClientNumerator{
         //on définit les initiales à rechercher dans la bdd
         $repo = $em->getRepository("MesClicsEspaceClientBundle:Client");
         $initials = substr($client->getAlias(), 0, 3);
-        $last_numero_full = $repo->lastClientBeginningWith($initials)->getNumero();
+        $last_numero_client = $repo->lastClientBeginningWith($initials);
+        if($last_numero_client){
+            $last_numero_full = $last_numero_client->getNumero();
+        }
 
         $next_numero = '001';
-        // $last_numero_full = $repo->lastClientNumeroBeginningWith($initial)->getNumero();
+        
         if(isset($last_numero_full)){
             $last_numero = intval(substr($last_numero_full, 4));
         }
