9e5ca68bb4b1bf06c1d8fdaba71012c76cc6c28e
MesClics <mesclics@hotmail.com>
2019-06-27 15:13:29 +0200

    EntityType form-row added to ContratType

--- Form/ContratType.php
+++ Form/ContratType.php
@@ -3,30 +3,50 @@
 namespace MesClics\EspaceClientBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use MesClics\EspaceClientBundle\Entity\Projet;
+use MesClics\EspaceClientBundle\Entity\Contrat;
+use MesClics\EspaceClientBundle\Form\ProjetType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use MesClics\EspaceClientBundle\Entity\Contrat;
 use MesClics\EspaceClientBundle\Repository\ProjetRepository;
-use MesClics\EspaceClientBundle\Form\ProjetType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class ContratType extends AbstractType
 {
+    protected $client;
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $client = $options['client'];
+        $this->client = $options['client'];
         $builder
         ->add('type', TextType::class)
         ->add('dateSignature', DateTimeType::class, array('required' => false))
+        ->add('projets', EntityType::class, array(
+            'class' => 'MesClicsEspaceClientBundle:Projet',
+            'query_builder' => function(ProjetRepository $repo){
+                return $repo->getProjetsWithNoContratQB($this->client);
+            },
+            'property_path' => 'projets',
+            'choice_label' => function(Projet $projet){
+                return $projet->getSelectLabel();
+            },
+            'choice_attr' => function(Projet $projet, $key, $index){
+                return ['class' => 'oocss-form-input-button',
+                        'title' => $projet->getNom()];
+            },
+            'expanded' => false,
+            'multiple' => true,
+            'required' => false
+        ))
         ->add('submit', SubmitType::class)
         ;
     }
